55ed9530a21ac8b322e8d742ec96e55816b62abd
Trushant Adeshara <trushant@umich.edu>
2024-11-15 14:14:30 -0500

    add docker container with ROS2 Humble layer on L4T base layer along with dummy image publisher

---
+++ util_scripts/ros2_ws/src/pallet_insight/launch/dummy_publisher.launch.py
@@ -0,0 +1,23 @@
+# Launch file for the dummy publisher
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    package_share_directory = get_package_share_directory('pallet_insight')
+    image_folder = os.path.join(package_share_directory, 'images')
+
+    return LaunchDescription([
+        Node(
+            package='pallet_insight',
+            executable='dummy_publisher',
+            name='dummy_publisher',
+            output='screen',
+            parameters=[
+                {'image_folder': 'images'},
+                {'publish_interval': 1.0},
+            ]
+        ),
+    ])
+
---
+++ util_scripts/run_ros2_container.sh
@@ -0,0 +1,1 @@
+docker run -it -v /home/trushant/pallet_insight_ros2/util_scripts/ros2_ws:/ros2_ws test:v0.0
